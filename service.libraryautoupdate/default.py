953c357f5eafac4ff312a831fdec898386578428
Rechi <Rechi@users.noreply.github.com>
2015-07-28 19:05:14 +0200

    [service.libraryautoupdate] remove

--- service.libraryautoupdate/default.py
+++
@@ -1,6 +0,0 @@
-import xbmc
-from service import AutoUpdater
-
-#run the program
-xbmc.log("Update Library Service starting...")
-AutoUpdater().runProgram()
--- service.libraryautoupdate/disclaimer.py
+++
@@ -1,8 +0,0 @@
-import xbmc
-import xbmcaddon
-import xbmcgui
-addon_id = "service.libraryautoupdate"
-Addon = xbmcaddon.Addon(addon_id)
-
-#show the disclaimer
-xbmcgui.Dialog().ok(Addon.getLocalizedString(30031),"",Addon.getLocalizedString(30032),Addon.getLocalizedString(30033))
